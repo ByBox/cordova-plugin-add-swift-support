dbc1b1c6a641094b520e7a332429091d071ca81a
Dave Osborne <david.osborne@bybox.com>
2018-01-29 07:51:01 +0000

    Cordova CLI dependency removed.
    Removed requirement for Cordova CLI to be used, just use the Cordova hooks.

--- src/main.js
+++ src/main.js
@@ -24,11 +24,7 @@ export default (context) => {
   const projectRoot = context.opts.projectRoot;
   const glob = context.requireCordovaModule('glob');
 
-  // This script has to be executed depending on the command line arguments, not
-  // on the hook execution cycle.
-  if ((context.hook === 'after_platform_add' && context.cmdLine.includes('platform add')) ||
-  (context.hook === 'after_prepare' && context.cmdLine.includes('prepare')) ||
-  (context.hook === 'after_plugin_add' && context.cmdLine.includes('plugin add'))) {
+  if (context.hook === 'after_platform_add' || context.hook === 'after_prepare' || context.hook === 'after_plugin_add') {
     getPlatformVersionsFromFileSystem(context, projectRoot).then((platformVersions) => {
       const IOS_MIN_DEPLOYMENT_TARGET = '7.0';
       const platformPath = path.join(projectRoot, 'platforms', 'ios');
